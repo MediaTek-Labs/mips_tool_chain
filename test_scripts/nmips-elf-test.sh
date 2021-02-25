7b81cd65cd0dd9ca9603e90bcb53c029ae5dbb3d
farazs-github <faraz.shahbazker@gmail.com>
2021-02-25 17:13:26 +0800

    Add libstdc++ testsuite to test script

--- test_scripts/nmips-elf-test.sh
+++ test_scripts/nmips-elf-test.sh
@@ -69,10 +69,15 @@ for cfg in "${configs[@]}"; do
 done
 fi
 
+configs+=(
+    "mips-sim-mti32/-m32/-EL/-msoft-float/-mno-gpopt/-fno-rtti/-fno-exceptions"
+    "mips-sim-mti32/-m32/-EL/-msoft-float/-mno-gpopt/-fno-rtti/-fno-exceptions/-nortti-libstdc++"
+)
+
 # manipulate the test_installed script to generate a modified site.exp
 sed 's|^set GCC_UNDER_TEST.*$|set GCC_UNDER_TEST \"'${TOOLCHAIN}'/bin/${target+$target-}gcc\"\nset HOSTCC \"gcc\"\nset HOSTCFLAGS \"\"|' $SRCDIR/gcc/contrib/test_installed > $SRCDIR/gcc/contrib/test_installed.g++"$$"
 chmod +x $SRCDIR/gcc/contrib/test_installed.g++"$$"
-if [ $DO = "g++" -o $DO = "both" -o $DO = "all" ]; then
+if [ $DO = "g++" -o $DO = "all" ]; then
 for cfg in "${configs[@]}"; do
     name="gxx_"`echo ${cfg#*/} | tr -d - | tr -d =  | tr / _`
     
@@ -85,6 +90,34 @@ for cfg in "${configs[@]}"; do
 done
 fi
 
+configs=(
+    "mips-sim-mti32/-m32/-EL/-msoft-float"
+    "mips-sim-mti32/-m32/-EL/-msoft-float/-mno-gpopt"
+    "mips-sim-mti32/-m32/-EL/-msoft-float/-nortti-libstdc++"
+    "mips-sim-mti32/-m32/-EL/-msoft-float/-mno-gpopt/-nortti-libstdc++"
+)
+
+# manipulate the previously generated test_installed.g++ script to generate a modified site.exp
+sed 's|^set CFLAGS.*$|set CFLAGS \"-Os -ffunction-sections -fdata-sections\"|' $SRCDIR/gcc/contrib/test_installed > $SRCDIR/gcc/contrib/test_installed.libstdc++"$$"
+sed -i 's|^set CXXFLAGS.*$|set CXXFLAGS \"-Os -ffunction-sections -fdata-sections\"|' $SRCDIR/gcc/contrib/test_installed.libstdc++"$$"
+sed -i 's|^set GCC_UNDER_TEST.*$|set GCC_UNDER_TEST \"'${TOOLCHAIN}'/bin/${target+$target-}gcc\"\nset HOSTCC \"gcc\"\nset HOSTCFLAGS \"\"|' $SRCDIR/gcc/contrib/test_installed.libstdc++"$$"
+sed -i 's|^set srcdir.*$|set srcdir \"'$SRCDIR'/gcc/libstdc++-v3/testsuite\"|' $SRCDIR/gcc/contrib/test_installed.libstdc++"$$" 
+sed -i 's|^exit 0$|runtest --tool libstdc++ ${1+"$@"}\nexit 0|g' $SRCDIR/gcc/contrib/test_installed.libstdc++"$$"
+chmod +x $SRCDIR/gcc/contrib/test_installed.libstdc++"$$"
+if [ $DO = "libstdc++" -o $DO = "all" ]; then
+for cfg in "${configs[@]}"; do
+    name="libstdcxx_"`echo ${cfg#*/} | tr -d - | tr -d =  | tr / _`
+    
+    mkdir $name
+    pushd $name
+    rm -Rf *
+
+    PATH=$TOOLCHAIN/bin:$HOSTTOOLS/bin:$SRCDIR/dejagnu:$PATH $SRCDIR/gcc/contrib/test_installed.libstdc++"$$" --without-gfortran --without-objc --without-gcc --without-g++ --prefix=$TOOLCHAIN --target=nanomips-elf --target_board=$cfg SIM=$TOOLCHAIN/bin/nanomips-elf-run -v -v -v  $4 &> test.log &
+    popd
+done
+fi
+
+
 wait
 # cleanup
-rm -f $SRCDIR/gcc/contrib/test_installed.gcc"$$" $SRCDIR/gcc/contrib/test_installed.g++"$$"
+rm -f $SRCDIR/gcc/contrib/test_installed.gcc"$$" $SRCDIR/gcc/contrib/test_installed.g++"$$" $SRCDIR/gcc/contrib/test_installed.libstdc++"$$"
